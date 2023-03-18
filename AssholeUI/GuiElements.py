6d1ea0a9111d171c7ac773e1debfc0d01412c10b
Richard Kilgour <richard.kilgour@gmail.com>
2023-03-18 21:09:57 +0100

    Expliclty set pos for GUI elements

--- AssholeUI/GuiElements.py
+++ AssholeUI/GuiElements.py
@@ -1,8 +1,11 @@
 import pygame
 
+
 class PlayerNameLabel(pygame.sprite.Sprite):
-    def __init__(self, name):
+    def __init__(self, name, x_pos, y_pos):
         pygame.sprite.Sprite.__init__(self)
+        self.x_pos = x_pos
+        self.y_pos = y_pos
         self.text = None
         self.font = pygame.font.SysFont("Arial", 24)
         self.set_text(name)
@@ -14,16 +17,20 @@ class PlayerNameLabel(pygame.sprite.Sprite):
         self.image.fill(pygame.color.THECOLORS['white'])
         self.image.blit(text, [0, 0])
         self.rect = self.image.get_rect()
+        self.rect.x = self.x_pos
+        self.rect.y = self.y_pos
 
 
 
 # TODO: Make a generic abstract button
 class PassButton(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, x_pos, y_pos):
         pygame.sprite.Sprite.__init__(self)
         self.font = pygame.font.SysFont("Arial", 24)
         text = self.font.render('PASS', 1, (0, 0, 0))
-        self.image = pygame.Surface((120, 50))
+        self.image = pygame.Surface((120, 80))
         self.image.fill(pygame.color.THECOLORS['white'])
         self.image.blit(text, [0, 0])
         self.rect = self.image.get_rect()
+        self.rect.x = x_pos
+        self.rect.y = y_pos
