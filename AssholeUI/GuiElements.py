4673e879a7a3ed13e66b2f2e52993dc67dfa34b3
Richard Kilgour <richard.kilgour@gmail.com>
2023-03-24 19:02:17 +0100

    Border and Highlight the Pass button

--- AssholeUI/GuiElements.py
+++ AssholeUI/GuiElements.py
@@ -23,13 +23,35 @@ class PlayerNameLabel(pygame.sprite.Sprite):
 
 # TODO: Make a generic abstract button
 class PassButton(pygame.sprite.Sprite):
-    def __init__(self, x_pos, y_pos):
+    def __init__(self, x_pos, y_pos, width = 120, height = 40):
         pygame.sprite.Sprite.__init__(self)
         self.font = pygame.font.SysFont("Arial", 24)
-        text = self.font.render('PASS', 1, (0, 0, 0))
-        self.image = pygame.Surface((120, 80))
-        self.image.fill(pygame.color.THECOLORS['white'])
-        self.image.blit(text, [0, 0])
+        self.text = self.font.render('PASS', 1, (0, 0, 0))
+        self.x = x_pos
+        self.y = y_pos
+        self.border = 2
+        self.highlighted = False
+        self.width = width
+        self.height = height
+        self.render()
+
+
+    def highlight(self, highlight):
+        if self.highlighted != highlight:
+            self.highlighted = highlight
+            self.render()
+
+
+    def render(self):
+        self.image = pygame.Surface((self.width, self.height))
+        if self.highlighted:
+            self.image.fill(pygame.color.THECOLORS['yellow'])
+        else:
+            self.image.fill(pygame.color.THECOLORS['black'])
+
+        pygame.draw.rect(self.image, pygame.color.THECOLORS['white'],
+                         (self.border, self.border, self.width - 2 * self.border, self.height - 2 * self.border))
+        self.image.blit(self.text, [0, 0])
         self.rect = self.image.get_rect()
-        self.rect.x = x_pos
-        self.rect.y = y_pos
+        self.rect.x = self.x
+        self.rect.y = self.y
